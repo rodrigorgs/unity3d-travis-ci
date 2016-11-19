369c1cccd50eb688c4cc7a76315003d6a1fd5f83
Rodrigo Rocha <rodrigorgs@gmail.com>
2016-11-19 10:19:00 -0300

    Add terrain and cap.

---
+++ Assets/Init.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics.gravity.Set (0, -5.0f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
